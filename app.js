0fdfd2013fe2a9321555afe59ad04e6b05e6d0d2
Edward Almanzar <eddeveloper@hotmail.com>
2018-01-04 01:32:04 -0500

    POST routes, setup a DEMON, Postman, body-parser and more.
    Add a demon with;
    >npm install -g nodemon
    to run> nodemon app.js
    
    Install Postman to make http petitions.
    
    Install body-parser to read the body for the petitions received from the browser;
    >npm install body-parser

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+//to read the petitions data that comes in json || urlencoded
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 // Mocking JSON
 const places = [
+	{
+		'title': 'Office Menteware',
+		'description': 'Best the best best ',
+		'address': '133 West 521 ST, NY'
+	},
 	{
 		'title': 'Office Menteware',
 		'description': 'Best the best best ',
@@ -28,6 +38,19 @@ app.get('/',(req, res)=>{
 	res.json(places);
 
 });
+
+app.post('/',(req,res)=>{
+// use the property body from he object request, it is fill by body-parser
+	// res.json(req.body);
+	res.json(req.body.nombre);
+	// NOTE: res.json(req.body.nombre)
+//  Make a petiion POST with this format:
+// {
+// 	"nombre": "Almanzar"
+// }
+// It will retur as: 	"Almanzar"
+})
+
 
 
 
