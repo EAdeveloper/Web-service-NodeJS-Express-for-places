f96020944d3633a7a5a3541c1a3e0c47bfb285d1
Edward Almanzar <eddeveloper@hotmail.com>
2018-01-06 09:46:31 -0500

    POST, Create Places
    Using Posyman making a POST petition at http://localhost:3000/places
    Should create the following in the DB;
       title: "Menteware Office",
        description: "Best software company",
        acceptsCreditCard: true,
        openHour: 0,
        closeHour: 24

--- api/app.js
+++ api/app.js
@@ -4,11 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
+const Place = require('./models/Place');
+
 const db = require('./config/database');
 db.connect();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -21,8 +21,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+
+
+// CRUD
+app.post('/places',(req, res)=>{
+    
+    Place.create({
+    // title:             req.body.title,
+    // description:       req.body.description,
+    // acceptsCreditCard: req.body.acceptsCreditCard,
+    // openHour:          req.body.openHour,
+    // closeHour:         req.body.closeHour
+    title: "Menteware Office",
+    description: "Best software company",
+    acceptsCreditCard: true,
+    openHour: 0,
+    closeHour: 24
+  }).then(doc=>{
+      res.json(doc)
+    }).catch(err=>{
+      console.log(err);
+      res.json(err);
+    });
+})
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,8 +62,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  
-  res.json('err');
+  res.json(err);
 
 });
 
