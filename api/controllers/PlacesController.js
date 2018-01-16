601d007cd9c9669def9ba9969e5e9b1ea501eca5
Edward Almanzar <eddeveloper@hotmail.com>
2018-01-16 17:01:53 -0500

    Controllers, to delegate the functions and logic

---
+++ api/controllers/PlacesController.js
@@ -0,0 +1,97 @@
+const Place = require('../models/Place');
+
+function index(req, res){
+	  // Show all places
+	Place.find({}).then(doc=>{
+    	res.json(doc);
+      }).catch(err=>{
+        console.log(err);
+        res.json(err);
+    });
+
+}
+
+function create(req, res){
+	Place.create({
+    title:             req.body.title,
+    description:       req.body.description,
+    acceptsCreditCard: req.body.acceptsCreditCard,
+    openHour:          req.body.openHour,
+    closeHour:         req.body.closeHour
+    // title: "Menteware Office",
+    // description: "Best software company",
+    // acceptsCreditCard: true,
+    // openHour: 0,
+    // closeHour: 24
+  }).then(doc=>{
+      res.json(doc)
+    }).catch(err=>{
+      console.log(err);
+      res.json(err);
+    });
+
+}
+
+// Show only one place. Wildcards :id
+function show(req, res){
+	// res.json(req.params.id);
+  // Place.findOne({})
+    Place.findById(req.params.id).then(doc=>{
+      res.json(doc);
+    }).catch(err=>{
+      console.log(err);
+      res.json(err);
+    });
+	
+}
+
+
+function update(req, res){
+	// Place.findById(req.params.id)
+    //   .then(doc=>{
+    //     doc.title =              req.body.title;
+    //     doc.description =        req.body.description;
+    //     doc.acceptsCreditCard =  req.body.acceptsCreditCard;
+    //     doc.openHour =           req.body.openHour;
+    //     doc.closeHour =          req.body.closeHour;
+    //     doc.save();
+    //   })
+    let attributes = ['title','description', 'acceptsCreditCard',
+                    'openHour', 'closeHour'];
+    let placeParams = {}
+    attributes.forEach(attr=>{
+      if(Object.prototype.hasOwnProperty.call(req.body, attr))
+        placeParams[attr] = req.body[attr];
+    });             
+    // Place.update({'_id': req.params.id},placeParams).then(doc=>{
+    // Place.findOneAndUpdate({'_id': req.params.id},placeParams,{new: true})
+    Place.findByIdAndUpdate(req.params.id, placeParams,{new: true})
+      .then(doc=>{
+        res.json(doc);
+      }).catch(err=>{
+        console.log(err);
+        res.json(err);
+      });
+	
+}
+
+function destroy(req, res){
+	Place.findByIdAndRemove(req.params.id).then(doc=>{
+      res.json({});
+    }).catch(err=>{
+      console.log(err);
+      res.json(err);
+    });
+	
+}
+
+// module.exports = {
+// 	index: index,
+// 	create: create,
+// 	show: show,
+// 	update: update,
+// 	destroy: destroy
+// }
+
+// Same code above uisng the shortkey properties from ecmascript 6
+module.exports = {index, show, create, destroy, update};
--- api/routes/places.js
+++ api/routes/places.js
@@ -1,89 +1,18 @@
 const express = require('express');
-const Place = require('../models/Place');
+
+const placesController = require('../controllers/PlacesController');
 
 let router = express.Router();
 
 router.route('/')
-  .get((req,res)=>{
-     // Show all places
-    Place.find({})
-      .then(doc=>{
-        res.json(doc);
-      }).catch(err=>{
-        console.log(err);
-        res.json(err);
-      });
-  })
-  .post((req, res)=>{
-    Place.create({
-    title:             req.body.title,
-    description:       req.body.description,
-    acceptsCreditCard: req.body.acceptsCreditCard,
-    openHour:          req.body.openHour,
-    closeHour:         req.body.closeHour
-    // title: "Menteware Office",
-    // description: "Best software company",
-    // acceptsCreditCard: true,
-    // openHour: 0,
-    // closeHour: 24
-  }).then(doc=>{
-      res.json(doc)
-    }).catch(err=>{
-      console.log(err);
-      res.json(err);
-    });
-})
+  .get(placesController.index)
+  .post(placesController.create)
 
 // Show only one place. Wildcards :id
 router.route('/:id')
-  .get((req,res)=>{
-  // res.json(req.params.id);
-  // Place.findOne({})
-    Place.findById(req.params.id)
-      .then(doc=>{
-      res.json(doc);
-    }).catch(err=>{
-      console.log(err);
-      res.json(err);
-    })
-  })
-
-  .put((req, res)=>{
-    // Place.findById(req.params.id)
-    //   .then(doc=>{
-    //     doc.title =              req.body.title;
-    //     doc.description =        req.body.description;
-    //     doc.acceptsCreditCard =  req.body.acceptsCreditCard;
-    //     doc.openHour =           req.body.openHour;
-    //     doc.closeHour =          req.body.closeHour;
-    //     doc.save();
-    //   })
-    let attributes = ['title','description', 'acceptsCreditCard',
-                    'openHour', 'closeHour'];
-    let placeParams = {}
-    attributes.forEach(attr=>{
-      if(Object.prototype.hasOwnProperty.call(req.body, attr))
-        placeParams[attr] = req.body[attr];
-    });             
-    // Place.update({'_id': req.params.id},placeParams).then(doc=>{
-    // Place.findOneAndUpdate({'_id': req.params.id},placeParams,{new: true})
-    Place.findByIdAndUpdate(req.params.id, placeParams,{new: true})
-      .then(doc=>{
-        res.json(doc);
-      }).catch(err=>{
-        console.log(err);
-        res.json(err);
-      });
-  })
-
-  .delete((req, res)=>{
-    Place.findByIdAndRemove(req.params.id)
-    .then(doc=>{
-      res.json({});
-    }).catch(err=>{
-      console.log(err);
-      res.json(err);
-    })
-  });
+  .get(placesController.show)
+  .put(placesController.update)
+  .delete(placesController.destroy)
+  
 
   module.exports= router;
