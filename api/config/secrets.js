1855baf1b63a005c807d984ffdcc9afc05f2bb59
Edward Almanzar <eddeveloper@hotmail.com>
2018-01-18 17:02:06 -0500

    Upload Images to the Cloud, using Cloudinary
    $ npm install cloudinary
    
    and Setup cloudinary uploader

---
+++ api/config/secrets.js
@@ -0,0 +1,17 @@
+module.exports={
+	cloudinary:{
+		apiKey: '699441845556732',
+		cludName: 'placesmenteware',
+		apiSecret: 'pN1AD7gSm1FDunFYG0XTm98xjf4'
+	}
+}
+
+
+
+// module.exports={
+// 	cloudinary:{
+// 		apiKey: 'xxxxxxxxxxxxxxxxxxxxxx',
+// 		cludName: 'xxxxxxxxxxxxxxxxxxxxxx',
+// 		apiSecret: 'xxxxxxxxxxxxxxxxxxxxxx'
+// 	}
+// }
