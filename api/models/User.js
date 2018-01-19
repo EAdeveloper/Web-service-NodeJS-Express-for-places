5b405fe8e5680282dc858214bf09166ba06bd169
Edward Almanzar <eddeveloper@hotmail.com>
2018-01-19 11:26:38 -0500

    Encrypt passwords with Bcrypt
    $ npm install mongoose-bcrypt
    
    //mongoose-bcrypt adds an encrypt field "password" and a set of methods like
    // verify password, encrypt password etc.

--- api/models/User.js
+++ api/models/User.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const mongooseBcrypt = require('mongoose-bcrypt');
 
 
 
 let userSchema = new mongoose.Schema({
 	email:{
 		type: String,
+		require: true,
 		unique: true
 	},
 	name: String,
@@ -13,6 +15,11 @@ let userSchema = new mongoose.Schema({
 		default: false
 	}
 });
+
+
+//mongoose-bcrypt adds an encrypt field "password" and a set of methods like
+// verify password, encrypt password etc.
+userSchema.plugin(mongooseBcrypt);
 
 const User = mongoose('User', userSchema);
 
